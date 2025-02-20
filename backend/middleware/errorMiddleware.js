a932f193cbb99efdbde22e0efe8317a90d05f94c
StilianSaka <stilisaka@gmail.com>
2025-02-20 12:04:04 +0100

    fixing code format

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,23 +1,22 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.origianlUrl}`);
-    res.status(404);
-    next(error);
+  const error = new Error(`Not Found - ${req.origianlUrl}`);
+  res.status(404);
+  next(error);
 };
 
 const errorHandler = (err, req, res, next) => {
-    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
 
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
 
-    if(err.name === 'CastError' && err.kind === 'ObjectId') {
-        message = `Resource not found`;
-        statusCode = 404;
-    }
-
-    res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production' ? '1F95E' : err.stack,
-    });
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? "1F95E" : err.stack,
+  });
 };
 
 export { notFound, errorHandler };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from 'react-router-dom'
-import { Provider } from 'react-redux';
-import store from './store';
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 //import 'bootstrap/dist/css/bootstrap.min.css'//
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,16 +23,15 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
-
 );
 
 reportWebVitals();
